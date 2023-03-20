e97691ea4f1d9f047f3d9d8a4eb1024c28f9a6d8
Maxime <maxime.rache@gmail.com>
2023-03-20 12:57:14 +0100

    Ajout code bouton + div test

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 const cells = []
 const game = document.querySelector("#game");
+const test = document.getElementById("test");
 
 for(let i = 0; i <= 8; i++) {
+
+    const cell = document.createElement("div");
+    cell.setAttribute("id", i);
+
+    cell.click = {
+        state : 0
+    }
+
+    game.appendChild(cell);
     cells.push(i);
-    const div = document.createElement("div");
-    div.setAttribute("id", i);
-    game.appendChild(div);
+}
+console.log(cells);
+
+for(let i = 0; i <= 8; i++) {
+
+    const cell = document.getElementById(`${i}`);
+
+
+    cell.addEventListener("click", function() {
+        cell.style.background = "#000000";
+        return cell.click.state = 1;
+    });
+
 }
 
-console.log(cells);
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", function() {
+
+    test.style.display = "block";    
+
+    /*if(cell.click.state = 1) {
+        cell.click.state = 0;
+        cell.style.background = "white";
+    }*/
+});
