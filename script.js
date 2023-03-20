7a413eeaa8e0b544892472937bef7cccd4c86932
Maxime <maxime.rache@gmail.com>
2023-03-20 13:16:44 +0100

    Modifications mineures

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-const cells = []
+const cells = [];
 const game = document.querySelector("#game");
 const test = document.getElementById("test");
+
+let toto = [
+    [0, 0]
+];
 
 for(let i = 0; i <= 8; i++) {
 
     const cell = document.createElement("div");
     cell.setAttribute("id", i);
-
-    cell.click = {
-        state : 0
-    }
-
     game.appendChild(cell);
+
     cells.push(i);
+    
+    /*
+    toto[i][0].push(i);
+    toto[i][1].push(0);
+    */
 }
-console.log(cells);
+
+console.log(toto);
 
 for(let i = 0; i <= 8; i++) {
 
-    const cell = document.getElementById(`${i}`);
+    const toto = document.getElementById(`${i}`);
 
-
-    cell.addEventListener("click", function() {
-        cell.style.background = "#000000";
-        return cell.click.state = 1;
+    toto.addEventListener("click", function() {
+        cell.cell[i].state = 1;
     });
+
+    if(cell.cell[i].state == 1) {
+        toto.style.background = "#000000";
+    }
 
 }
 
 const resetButton = document.getElementById("reset");
 resetButton.addEventListener("click", function() {
 
-    test.style.display = "block";    
-
-    /*if(cell.click.state = 1) {
-        cell.click.state = 0;
-        cell.style.background = "white";
-    }*/
+    test.style.display = "block";
 });
